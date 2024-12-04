cafc74a4b680b95c0cf8a63d543ec71b7385f58e
Tejasree-S27 <stejasree348@gmail.com>
2024-12-04 13:48:37 +0530

    Add files via upload

---
+++ Multiple.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Multiple
+{
+    public static void main(String args[])
+    {
+        int n=24;
+        if((n&3)==0)
+        {
+        System.out.println("Multiple of 4");
+        }
+        else
+        {
+        System.out.println("Not a Multiple of 4");
+        }
+    }    
+}
